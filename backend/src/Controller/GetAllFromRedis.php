6e46a8964bb67f95537bb44a101f21d7f3622863
YARTEMIY <yarovikov2003@gmail.com>
2024-05-05 14:58:39 +0500

    test documentation

--- backend/src/Controller/GetAllFromRedis.php
+++ backend/src/Controller/GetAllFromRedis.php
@@ -1,7 +1,10 @@
 <?php
 
-namespace App\Controller;
+/**
+ * @OA\Info(title="API Documentation", version="1.0")
+ */
 
+namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -10,6 +13,25 @@ use Predis;
 
 class GetAllFromRedis extends AbstractController
 {
+    /**
+     * Retrieves all users from Redis.
+     * 
+     * This endpoint retrieves all users stored in Redis.
+     *
+     * @OA\Get(
+     *     path="/api/getall",
+     *     summary="Retrieve all users from Redis",
+     *     @OA\Response(response="200", description="Successful operation")
+     * )
+     * @OA\Post(
+     *     path="/api/getall",
+     *     summary="Retrieve all users from Redis",
+     *     @OA\Response(response="200", description="Successful operation")
+     * )
+     * 
+     * @Route("/api/getall", methods={"GET", "POST"})
+     * @OA\PathItem()
+     */
     #[Route('api/getall', methods: ['GET', 'POST'])]
     public function getAll(): Response
     {
